e3d351e950d005cd4faa5a330f944ddbbe36a3e0
lurbano <lurbano@earthsciweb.org>
2022-11-04 13:47:30 -0500

    os

--- alm.py
+++ alm.py
@@ -1,8 +1,7 @@
+
+# sound effects from https://sound-effects.bbcrewind.co.uk/search
+
 import time
 import os
-import board
-import digitalio
-import pygame
-pygame.mixer.init()
-pygame.mixer.music.load("bbc_comedy-sou_07005034.mp3")
-pygame.mixer.music.play()
+
+os.system("bbc_comedy-sou_07005034.mp3")
