3ed9fb8b5b255b476762b3d7cb10c646e1ed8e62
lurbano <lurbano@earthsciweb.org>
2022-11-19 20:28:15 -0600

    loop

---
+++ loop.py
@@ -0,0 +1,12 @@
+# sound effects from https://sound-effects.bbcrewind.co.uk/search
+
+import time
+import os
+
+os.system('amixer cset numid=1 90%')
+os.system("cvlc --play-and-exit bbc_comedy-sou_07005034.mp3")
+
+
+while True:
+    time.sleep(5)
+    os.system("cvlc --play-and-exit bbc_comedy-sou_07005034.mp3")
