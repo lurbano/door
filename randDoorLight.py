ae0cdf525d4e8b421601c38e80bf1053f253087a
lurbano <lurbano@earthsciweb.org>
2022-11-21 13:34:27 -0600

    led

---
+++ randDoorLight.py
@@ -0,0 +1,50 @@
+
+# Note: Julian's file form his portal repository.
+# Randomly play files in the ./sound directory
+
+import time
+import os
+import subprocess
+import board
+import digitalio
+import random
+import neopixel
+
+nPix = 20
+pixels = neopixel.NeoPixel(board.D21, 20)
+
+
+
+os.system('amixer cset numid=1 100%')
+os.system("cvlc --play-and-exit /home/pi/door/sounds/portal_start.mp3")
+
+filelist = os.listdir("/home/pi/door/sounds")
+# subprocess.Popen('amixer cset numid=1 100%', shell=True)
+# subprocess.Popen("cvlc --play-and-exit /home/pi/portal/portal_start.mp3", shell=True)
+
+s1 = digitalio.DigitalInOut(board.D17)
+s1.direction = digitalio.Direction.INPUT
+s1.pull = digitalio.Pull.DOWN
+
+isopen = s1.value
+
+
+while True:
+        #if (s1.value) == True:
+        #       print("closed")
+        #else:
+        #        print("open")
+        if (s1.value != isopen):
+                isopen = s1.value
+                if isopen == True:
+                        print("closed")
+                        for i in range(nPix):
+                            pixels[i] = (100,0,0)
+                else:
+                        print("open")
+                        soundFile = f"/home/pi/door/sounds/{random.choice(filelist)}"
+                        os.system(f"cvlc --play-and-exit {soundFile}")
+                        for i in range(nPix):
+                            pixels[i] = (0,100,0)
+
+        time.sleep(0.05)
